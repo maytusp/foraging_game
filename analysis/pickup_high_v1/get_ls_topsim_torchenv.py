fix bug on LS calculation (bug since nov)

analysis/pickup_high_v1/get_ls_topsim_torchenv.py
@@ -1,3 +1,5 @@
+# new code updated 28 march 2026
+
 import pickle
 import numpy as np
 import torch
@@ -15,7 +17,6 @@ def load_trajectory(file_path):
     with open(file_path, "rb") as f:
         log_data = pickle.load(f)
     return log_data
-
 
 def get_episode_length(log_s_messages: np.ndarray) -> int:
     """
@@ -43,13 +44,14 @@ def get_mode_episode_length(log_data) -> int:
     mode_length = counts.most_common(1)[0][0]
     return mode_length
 
-def extract_data(log_data):
+def extract_data_for_ls(log_data):
     # Message length is the mode of the episode length
     mode_length = get_mode_episode_length(log_data)
     message_data = {"agent0": [], "agent1": []}
     attribute_data = []
     scores = {"agent0": [], "agent1": []}
     item_locs = {"agent0": [], "agent1": []}
+    communication_steps = 6
 
     for episode_id, data in log_data.items():
         log_s_messages = data["log_s_messages"]
@@ -62,12 +64,42 @@ def extract_data(log_data):
         # Compute this episode's actual length
         ep_len = get_episode_length(log_s_messages)
 
-        # Option 1: only keep episodes whose length == mode_length
-        if ep_len == mode_length and who_see_target == 0:
-            messages = log_s_messages[:ep_len, 0].flatten()
+
+        if ep_len >= communication_steps and who_see_target == 0:
+            messages = log_s_messages[:communication_steps, 0].flatten()
+            message_data["agent0"].append(messages)
+        
+    return message_data
+
+def extract_data_for_topsim(log_data):
+    # Message length is the mode of the episode length
+    mode_length = get_mode_episode_length(log_data)
+    message_data = {"agent0": [], "agent1": []}
+    attribute_data = []
+    scores = {"agent0": [], "agent1": []}
+    item_locs = {"agent0": [], "agent1": []}
+    communication_steps = 6 # only consider the first 6 steps of communication, since that's the mode episode length for most models, and topsim requires all episodes to have the same length
+
+    for episode_id, data in log_data.items():
+        log_s_messages = data["log_s_messages"]
+        log_rewards = data["log_rewards"]
+        who_see_target = data["who_see_target"]
+        target_score = data["log_target_food_dict"]["score"]
+        target_loc = data["log_target_food_dict"]["location"]       # (2,)
+        distractor_score = data["log_distractor_food_dict"]["score"][0]
+        distractor_loc = data["log_distractor_food_dict"]["location"][0]  # (2,)
+        # Compute this episode's actual length
+        ep_len = get_episode_length(log_s_messages)
+
+        # This is only for topsim and posdis, since we need to align messages and attributes at each time step. For similarity, we can keep all episodes and pad messages to the same length.
+        if ep_len >= communication_steps and who_see_target == 0:
+            messages = log_s_messages[:communication_steps, 0].flatten()
             message_data["agent0"].append(messages)
             extract_attribute = [target_score, target_loc[0], target_loc[1]]
             attribute_data.append(extract_attribute)
+            # else:
+            #     extract_attribute = [distractor_score, distractor_loc[0], distractor_loc[1]]
+            #     attribute_data.append(extract_attribute)
             
     return message_data, attribute_data
 
@@ -122,37 +154,48 @@ def get_comp_scores(message_data, attribute_data, num_networks):
 
     return avg_topsim, avg_posdis, topsim_list, posdis_list
 
+
 def get_similarity(message_data, num_networks):
     sender_list = [i for i in range(num_networks)]
-    data = []
-    similarity_mat = np.zeros((num_networks,num_networks))
+    similarity_mat = np.zeros((num_networks, num_networks))
     n_samples = 1000000
     extracted_message = []
     receiver = 0
-    for sender in sender_list:
-        extracted_message.append(np.array(message_data[f"{sender}-{receiver}"]["agent0"]))
-        n_samples = min(extracted_message[sender].shape[0], n_samples)
 
+    for sender in sender_list:
+        msgs = message_data[f"{sender}-{receiver}"]["agent0"]   # keep as list
+        extracted_message.append(msgs)
+        n_samples = min(len(msgs), n_samples)
+
+    print("langsim n_samples =", n_samples)
+
+    if n_samples == 0:
+        raise ValueError("No langsim samples found.")
 
     for first_agent_id in range(len(sender_list)):
         for second_agent_id in range(len(sender_list)):
-            for i in range(n_samples):          
+            for i in range(n_samples):
                 m1 = extracted_message[sender_list[first_agent_id]][i]
                 m2 = extracted_message[sender_list[second_agent_id]][i]
 
-                m1 = [i for i in m1 if i != -1]
-                m2 = [i for i in m2 if i != -1]
+                m1 = [x for x in m1 if x != -1]
+                m2 = [x for x in m2 if x != -1]
 
-                dist = editdistance.eval(m1, m2) / max(len(m1), len(m2))
-               
-                similarity_mat[first_agent_id,second_agent_id] += (1 - dist)
+                denom = max(len(m1), len(m2))
+                if denom == 0:
+                    sim = 1.0
+                else:
+                    dist = editdistance.eval(m1, m2) / denom
+                    sim = 1 - dist
+
+                similarity_mat[first_agent_id, second_agent_id] += sim
 
     similarity_mat = similarity_mat / n_samples
     mask = np.ones_like(similarity_mat)
     mask[np.triu_indices_from(mask, k=0)] = 0
     avg_sim = np.sum(similarity_mat * mask) / np.sum(mask)
 
-    return similarity_mat , avg_sim
+    return similarity_mat, avg_sim
 
 
 
@@ -192,8 +235,8 @@ if __name__ == "__main__":
 
     checkpoints_dict = {
                         # "dec_ppo_invisible" : {"seed1":204800000, "seed2":204800000, "seed3":204800000},
-                        "pop_ppo_3net": {"seed1": 256000000, "seed2": 256000000, "seed3": 256000000}, # vis-com condition
-                        "pop_ppo_3net_invisible": {'seed1': 256000000, 'seed2': 256000000, 'seed3':256000000},
+                        # "pop_ppo_3net": {"seed1": 256000000, "seed2": 256000000, "seed3": 256000000}, # vis-com condition
+                        # "pop_ppo_3net_invisible": {'seed1': 256000000, 'seed2': 256000000, 'seed3':256000000},
                         # "pop_ppo_6net_invisible": {'seed1': 460800000, 'seed2': 460800000, 'seed3':460800000},
                         # "pop_ppo_9net_invisible": {'seed1': 512000000, 'seed2': 512000000, 'seed3':512000000},
                         # "pop_ppo_12net_invisible": {'seed1': 768000000, 'seed2': 768000000, 'seed3':768000000},
@@ -204,6 +247,7 @@ if __name__ == "__main__":
                         # "pop_sp_ppo_9net_invisible": {'seed1': 512000000, 'seed2': 512000000, 'seed3':512000000},
                         # "pop_sp_ppo_12net_invisible": {'seed1': 768000000, 'seed2': 768000000, 'seed3':768000000},
                         # "pop_sp_ppo_15net_invisible": {'seed1': 819200000, 'seed2': 819200000, 'seed3':819200000},
+                        "fc_ppo_100net_invisible": {'seed1': None, 'seed2': None, 'seed3':None},
                         }
     model2numnet = {
         "dec_ppo_invisible": 2,
@@ -219,6 +263,7 @@ if __name__ == "__main__":
         "pop_sp_ppo_9net_invisible": 9,
         "pop_sp_ppo_12net_invisible": 12,
         "pop_sp_ppo_15net_invisible": 15,
+        "fc_ppo_100net_invisible": 100,
     }
     compute_topsim = True
     cbar = False
@@ -228,13 +273,15 @@ if __name__ == "__main__":
         avg_sr_mat = np.zeros((num_networks,num_networks))
         per_agent_topsim = []
         per_agent_posdis = []
-        for seed in range(1,4):
+        for seed in range(1,2):
             ckpt_name = checkpoints_dict[model_name][f"seed{seed}"]
-            combination_name = f"grid5_img3_ni2_nw4_ms10_{ckpt_name}"
+            combination_name = "grid5_img3_ni2_nw4_ms30_comm_field5"
+            if ckpt_name is not None:
+                combination_name += f"_{ckpt_name}"
 
             print(f"{model_name}/{combination_name}")
             saved_fig_dir = f"plots/population/fc/sr_lang_sim"
-            saved_score_dir = f"../../logs/vary_n_pop/msg_len_mode/{model_name}/{combination_name}_seed{seed}"
+            saved_score_dir = f"../../logs/vary_n_pop/torch_100net/sr_lang_sim/{model_name}/{combination_name}_seed{seed}"
             saved_fig_path_langsim = os.path.join(saved_fig_dir, f"{model_name}_{combination_name}_seed{seed}_similarity.pdf")
             saved_fig_path_sr = os.path.join(saved_fig_dir, f"{model_name}_{combination_name}_seed{seed}_sr.pdf")
             os.makedirs(saved_fig_dir, exist_ok=True)
@@ -242,58 +289,52 @@ if __name__ == "__main__":
             mode = "test"
             if num_networks <= 2:
                 network_pairs = [f"{i}-{j}" for i in range(num_networks) for j in range(num_networks)]
+            elif num_networks == 100:
+                network_pairs = [f"{i}-0" for i in range(num_networks)]
             else:
                 network_pairs = [f"{i}-{j}" for i in range(num_networks) for j in range(i+1)]
+
             log_file_path = {}
             sr_dict = {}
             sr_mat = np.zeros((num_networks, num_networks))
             message_data = {}
-            attribute_data = {}
-            
-            # For Interchangeability
-            ic_numerator = []
-            ic_denominator = []
+
+            # For topsim and posdis calculation, we only keep episodes with length == mode_length, so we need separate dict to store the filtered data
+            # This is because topsim requires all episodes to have the same length
+            message_data_topsim = {} 
+            attribute_data_topsim = {}
+        
             
             for pair in network_pairs:
                 row, col = pair.split("-")
                 row, col = int(row), int(col)
                 print(f"loading network pair {pair}")
-                log_file_path[pair] =  f"../../logs/vary_n_pop/{model_name}/{pair}/{combination_name}/seed{seed}/mode_{mode}/normal/trajectory.pkl"
-                sr_dict[pair] = load_score(f"../../logs/vary_n_pop/{model_name}/{pair}/{combination_name}/seed{seed}/mode_{mode}/normal/score.txt")
-                sr_mat[row, col] = sr_dict[pair]["Success Rate"]
-                if row == col:
-                    ic_numerator.append(sr_dict[pair]["Success Rate"])
-                else:
-                    ic_denominator.append(sr_dict[pair]["Success Rate"])
-                # Load log data
+                log_file_path[pair] =  f"../../logs/vary_n_pop/torch_100net/langsim/{model_name}/{pair}/{combination_name}/seed{seed}/mode_{mode}/normal/trajectory.pkl"
+            
                 log_data = load_trajectory(log_file_path[pair])
 
-                # Prepare data for t-SNE
-                message_data[pair], attribute_data[pair] = extract_data(log_data)
+                message_data[pair] = extract_data_for_ls(log_data)
+                message_data_topsim[pair], attribute_data_topsim[pair] = extract_data_for_topsim(log_data)
 
 
-            ic = np.mean(ic_numerator) / np.mean(ic_denominator)
             similarity_mat, avg_sim = get_similarity(message_data, num_networks)
             print(f"Similarity score: {avg_sim} \n matrix: {similarity_mat}")
-            print(f"Interchangeability: {ic}")
+
             # plot_heatmap(similarity_mat, saved_fig_path_langsim)
             # plot_heatmap(sr_mat, saved_fig_path_sr)
             
             if compute_topsim:
-                avg_topsim, avg_posdis, topsim_list, posdis_list = get_comp_scores(message_data, attribute_data, num_networks)
-                # print(f"avg topsim = {avg_topsim}")
-                # print(f"avg posdis = {avg_posdis}")
+                avg_topsim, avg_posdis, topsim_list, posdis_list = get_comp_scores(message_data_topsim, attribute_data_topsim, num_networks)
                 per_agent_topsim += topsim_list
                 per_agent_posdis += posdis_list
-                # Save the variables
-                np.savez(os.path.join(saved_score_dir, "sim_scores.npz"), similarity_mat=similarity_mat, 
-                                                                        avg_sim=avg_sim, 
-                                                                        avg_topsim=avg_topsim, 
-                                                                        avg_posdis=avg_posdis,
-                                                                        per_agent_topsim=per_agent_topsim,
-                                                                        per_agent_posdis=per_agent_posdis,
-                                                                        sr_mat=sr_mat,
-                                                                        ic=ic)
+                # np.savez(os.path.join(saved_score_dir, "sim_scores.npz"), similarity_mat=similarity_mat, 
+                #                                                         avg_sim=avg_sim, 
+                #                                                         avg_topsim=avg_topsim, 
+                #                                                         avg_posdis=avg_posdis,
+                #                                                         per_agent_topsim=per_agent_topsim,
+                #                                                         per_agent_posdis=per_agent_posdis,
+                #                                                         sr_mat=sr_mat,
+                #                                                         ic=ic)
             avg_similarity_mat += similarity_mat
             avg_sr_mat += sr_mat
         print(f"Average topsim across all agents: {np.mean(per_agent_topsim)}, SE: {np.std(per_agent_topsim) / np.sqrt(len(per_agent_topsim))}")

scripts/torch_scoreg_layout/train_large_pop_fc.py
@@ -17,7 +17,7 @@ from environments.torch_scoreg_layout import TorchForagingEnv, EnvConfig, simple
 
 from utils.process_data import *
 from models.pickup_models import PPOLSTMCommAgent
-# python -m scripts.torch_scoreg_layout.train
+# CUDA_VISIBLE_DEVICES=0 python -m scripts.torch_scoreg_layout.train_large_pop_fc --no-agent-visible --num-networks 100 --seed 1
 @dataclass
 class Args:
     seed: int = 4
@@ -524,7 +524,7 @@ if __name__ == "__main__":
             explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
 
             # TRY NOT TO MODIFY: record rewards for plotting purposes
-            if args.visualize_loss and (global_step // args.num_envs) % args.log_every == 0:
+            if args.visualize_loss and (global_step // args.num_envs) % args.log_every == 0 and network_id < 30: # we only visualise some agents, 100 seems to be too much
                 writer.add_scalar(f"agent{network_id}/charts/learning_rate", optimizers[network_id].param_groups[0]["lr"], global_step)
                 writer.add_scalar(f"agent{network_id}/losses/value_loss", v_loss.item(), global_step)
                 writer.add_scalar(f"agent{network_id}/losses/action_loss", pg_loss.item(), global_step)