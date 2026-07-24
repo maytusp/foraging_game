b39c4c3f33d9f7ad30c96e9e0496c47483a17c44
maytusp <maytusp@gmail.com>
2026-07-24 16:05:44 +0100

    train ring and clq with 15 and 30 nets

---
+++ bash_script/eval_ls_sr_ws_s2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash --login
+#SBATCH -p gpuL             # A100 GPUs
+#SBATCH -G 1                  # 1 GPU
+#SBATCH -t 1-0                # Wallclock limit (1-0 is 1 day, 4-0 is the max permitted)
+#SBATCH -n 1                  # One Slurm task
+#SBATCH -c 12                  # 8 CPU cores available to the host code.
+                              # Can use up to 12 CPUs with an A100 GPU.
+                              # Can use up to 12 CPUs with an L40s GPU.
+
+# Latest version of CUDA
+
+cd ..
+SCRIPT_DIR="$(pwd)"
+echo "Script directory: $SCRIPT_DIR"
+
+source activate habitat
+# For WS Cannot Do this. Need --graph-structure....
+# # LS
+
+python -m scripts.torch_scoreg_layout.prepare_ls_struct --no-all-pairs --graph-structure ws_pairs_100 --num-networks 100 --model-name ws_ppo_100net_invisible --model-step 1200000000 --seed 2
+
+# SR 
+
+python -m scripts.torch_scoreg_layout.eval_batched_sr --all-pairs --num-networks 100 --model-name ws_ppo_100net_invisible --model-step 1200000000 --seed 2
+
---
+++ bash_script/tr_clq15-30.sh
@@ -0,0 +1,21 @@
+#!/bin/bash --login
+#SBATCH -p gpuL             # A100 GPUs
+#SBATCH -G 1                  # 1 GPU
+#SBATCH -t 2-0                # Wallclock limit (1-0 is 1 day, 4-0 is the max permitted)
+#SBATCH -n 1                  # One Slurm task
+#SBATCH -c 12                  # 8 CPU cores available to the host code.
+                              # Can use up to 12 CPUs with an A100 GPU.
+                              # Can use up to 12 CPUs with an L40s GPU.
+
+# Latest version of CUDA
+
+cd ..
+SCRIPT_DIR="$(pwd)"
+echo "Script directory: $SCRIPT_DIR"
+
+source activate habitat
+
+
+python -m scripts.torch_scoreg_layout.train_struct --social-network clq --comm_field 100 --no-agent-visible --num_networks 15 --seed 1 --no-self-play-option --total-timesteps 600000000 --checkpoint-root checkpoints/scoreg
+
+python -m scripts.torch_scoreg_layout.train_struct --social-network clq --comm_field 100 --no-agent-visible --num_networks 30 --seed 1 --no-self-play-option --total-timesteps 1000000000 --checkpoint-root checkpoints/scoreg
